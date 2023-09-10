cdaaa4b708b3b652624333bd44a55638ef08fc11
Ashish Rai <ashish.rai@appinventiv.com>
2023-09-10 20:31:36 +0530

    Adding Exceptions and Api Responses

---
+++ lib/data/response/api_response.dart
@@ -0,0 +1,19 @@
+import 'package:flutter_provider_mvvm/data/response/status.dart';
+
+class APIResponse<T> {
+  Status? status;
+  T? data;
+  String? message;
+
+  APIResponse(this.status, this.data, this.message);
+
+  APIResponse.loading() : status = Status.LOADING;
+  APIResponse.completed() : status = Status.COMPLETED;
+  APIResponse.error() : status = Status.ERROR;
+
+  @override
+  String toString() {
+    return "Status: $status \n Data: $data \n Message: $message";
+  }
+
+}
